c8a0673455a4d9a31cee76d2c1875df21b45ca72
dignifiedquire <me@dignifiedquire.com>
2020-05-08 14:47:15 +0200

    simplify rust bench

--- rust/benches/tcp_echo.rs
+++ rust/benches/tcp_echo.rs
@@ -1,7 +1,7 @@
 use async_std::net::{TcpListener, TcpStream};
+use async_std::prelude::*;
 use async_std::task;
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
-use futures::io::{AsyncReadExt, AsyncWriteExt};
 use futures::stream::StreamExt;
 
 const SIZE: u64 = 1000;
@@ -11,23 +11,27 @@ fn bench_throughput(c: &mut Criterion) {
     env_logger::from_env(env_logger::Env::default().default_filter_or("error")).init();
     let address = "localhost:11011";
 
-    let _server = task::block_on(async {
-        let listener = TcpListener::bind(&address).await.unwrap();
-        task::spawn(async move {
-            let mut incoming = listener.incoming();
-            while let Some(Ok(mut stream)) = incoming.next().await {
-                let clone = stream.clone();
-                task::spawn(async move {
-                    futures::io::copy(clone, &mut stream).await.unwrap();
-                });
-            }
-        });
-    });
+    let run_server = || {
+        task::block_on(async {
+            let listener = TcpListener::bind(&address).await.unwrap();
+            task::spawn(async move {
+                let mut incoming = listener.incoming();
+                while let Some(Ok(mut stream)) = incoming.next().await {
+                    task::spawn(async move {
+                        let mut read = stream.clone();
+                        let write = &mut stream;
+                        async_std::io::copy(&mut read, write).await.unwrap();
+                    });
+                }
+            })
+        })
+    };
 
     let mut group = c.benchmark_group("tcp-throughput");
     group.sample_size(10);
     group.throughput(Throughput::Bytes(SIZE * COUNT));
     group.bench_function("tcp echo one task", |b| {
+        let server = run_server();
         b.iter(|| {
             task::block_on(async move {
                 let data = vec![1u8; SIZE as usize];
@@ -47,9 +51,12 @@ fn bench_throughput(c: &mut Criterion) {
                 })
                 .await;
             })
-        })
+        });
+        task::block_on(async move { server.cancel().await });
     });
+
     group.bench_function("tcp echo two tasks", |b| {
+        let server = run_server();
         b.iter(|| {
             task::block_on(async move {
                 let data = vec![1u8; SIZE as usize];
@@ -70,9 +77,11 @@ fn bench_throughput(c: &mut Criterion) {
                     assert!(&buf[0] == &1u8);
                     assert!(&buf[buf.len() - 1] == &1u8);
                 });
-                futures::future::join(reader, writer).await;
+                reader.await;
+                writer.await;
             })
-        })
+        });
+        task::block_on(async move { server.cancel().await });
     });
     group.finish();
 }
